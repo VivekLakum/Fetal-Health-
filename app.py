97b7691f53b1fbbe65ed8c29ca8a36831986c306
Vivek Lakum <lakumvivek01@gmail.com>
2025-04-18 12:22:08 +0530

    app.py

--- app.py
+++ app.py
@@ -9,15 +9,30 @@ st.title("👶 Fetal Health Classifier")
 st.write("Please enter all 21 feature values below 👇")
 
 # List of all 21 features
-features = [
-    "baseline value", "accelerations", "fetal movement", "uterine contractions",
-    "prolongued decelerations", "light decelerations", "severe decelerations",
-    "abnormal short term variability", "mean short term variability",
-    "percentage abnormal long term variability", "histogram width", "histogram min",
-    "histogram max", "histogram number of peaks", "histogram number of zeroes",
-    "histogram mode", "histogram mean", "histogram median", "histogram variance",
-    "histogram tendency"
+features =[
+  'baseline value',
+  'accelerations',
+  'fetal_movement',
+  'uterine_contractions',
+  'light_decelerations',
+  'severe_decelerations',
+  'prolongued_decelerations',
+  'abnormal_short_term_variability',
+  'mean_value_of_short_term_variability',
+  'percentage_of_time_with_abnormal_long_term_variability',
+  'mean_value_of_long_term_variability',
+  'histogram_width',
+  'histogram_min',
+  'histogram_max',
+  'histogram_number_of_peaks',
+  'histogram_number_of_zeroes',
+  'histogram_mode',
+  'histogram_mean',
+  'histogram_median',
+  'histogram_variance',
+  'histogram_tendency'
 ]
+
 
 # Collect inputs
 inputs = []
